555a0c39c69edfd5867f9cb71543f11bb0caf540
minJUu-kiM <139558843+minJUu-kiM@users.noreply.github.com>
2025-05-29 10:56:16 +0900

    [Bronze III] Title: ACM 호텔, Time: 104 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+// 입력값이 하나일때
+//const input = require("fs").readFileSync("input.txt").toString().trim();
+
+//입력값 사이에 공백이 있을때  | 정수로 바꾸려면 .map(Number)
+//const input = fs.readFileSync("input.txt").toString().trim().split(" ").map(Number);
+
+// 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
+//const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\s/);
+
+//첫 번째 줄에 자연수 n을 입력받고, 그 다음줄부터 n개의 줄에 걸쳐 한 줄에 하나의 값을 입력받을 때
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+for (let i = 1; i < input.length; i++) {
+  const arr = input[i].split(" ").map(Number);
+  const h = arr[2] % arr[0] === 0 ? arr[0] : arr[2] % arr[0];
+  const w = Math.ceil(arr[2] / arr[0]);
+
+  const result = h.toString() + (w < 10 ? "0" + w.toString() : w.toString());
+  console.log(result);
+}
+
+// /dev/stdin
