b2e4178145b2907041f90c12f7a43c9758f8d057
minJUu-kiM <139558843+minJUu-kiM@users.noreply.github.com>
2025-06-01 21:14:58 +0900

    [Bronze II] Title: 나머지, Time: 80 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+// 입력값이 하나일때
+//const input = require("fs").readFileSync("input.txt").toString().trim();
+
+//입력값 사이에 공백이 있을때  | 정수로 바꾸려면 .map(Number)
+//const input = fs.readFileSync("input.txt").toString().trim().split(" ").map(Number);
+
+// 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
+//const input = require("fs").readFileSync("input.txt").toString().trim().split("\n");
+
+//첫 번째 줄에 자연수 n을 입력받고, 그 다음줄부터 n개의 줄에 걸쳐 한 줄에 하나의 값을 입력받을 때
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const restSet = new Set();
+
+for (let i = 0; i < input.length; i++) {
+  restSet.add(input[i] % 42);
+}
+
+console.log(restSet.size);
+
+// /dev/stdin
