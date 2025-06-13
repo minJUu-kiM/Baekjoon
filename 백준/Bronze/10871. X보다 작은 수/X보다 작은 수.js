560878723e4663795db3305df2b80df8c5a64aa3
minJUu-kiM <139558843+minJUu-kiM@users.noreply.github.com>
2025-06-13 20:03:51 +0900

    [Bronze V] Title: X보다 작은 수, Time: 96 ms, Memory: 11056 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 
-//3 9 -> 이런식으로 두가지 숫자 입력받았을때
-//const input = fs.readFileSync("input.txt").toString().trim().split(" ").map(Number);
-
-// 여러줄로 입력 받았을때때
 const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
